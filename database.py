cdf761b24585c3598a56311fe8863ef7fce37a2f
Uma03AI <upvlk03@gmail.com>
2026-02-12 21:47:44 +0530

    Fixed database environment variable issue

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
-DATABASE_NAME = os.getenv("ProjectNexus")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 client = AsyncIOMotorClient(MONGO_URL)
-database = client["ProjectNexus"]
+database = client["DATABASE_NAME"]
 
 # Example collection
 users_collection = database.get_collection("users")
